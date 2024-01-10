37cedf3fe3f94a50fa849daaed6278b9f1fb599a
André <137167063+AndreK-B06@users.noreply.github.com>
2024-01-10 13:43:33 +0100

    first
    test react comimit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className='app'>
-      <header className='navbar bg-primary text-secondary'>
-        <a href='/'>A blank slate</a>
+    <div className="app">
+      <header className="navbar bg-primary text-secondary">
+        <a href="/">A blank slate</a>
       </header>
 
-      <main className='main'>
+      <main className="main">
         <h1>A blank slate for you to use</h1>
       </main>
 
-      <footer className='footer bg-primary text-secondary'>
-
+      <footer className="footer bg-primary text-secondary">
         <nav>
           <ul>
-            <li><a href=""></a></li>
+            <li>
+              <a href=""></a>
+            </li>
           </ul>
         </nav>
 
         <p>Made by Kodehodet 2024</p>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
